e0da55323180ca7eed2b05d68ad00f5af0243bc6
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-05-17 12:35:41 +0200

    added OpenAPI 2 support

--- src/run.js
+++ src/run.js
@@ -5,46 +5,51 @@ const toJsonSchema = require("@openapi-contrib/openapi-schema-to-json-schema");
 const mergeAllOf = require("json-schema-merge-allof");
 const utils = require("./utils");
 
-const testSchemaFilePath = process.argv[2];
+const inputSchemaPath = process.argv[2];
 
 (async function () {
 
-    try {
+    // Load input schema, parse to object and resolve all $ref's.
+    let schemas = await $RefParser.dereference(inputSchemaPath);
 
-        // Load OpenAPI schema, parse to object and resolve all $ref's.
-        let schemas = await $RefParser.dereference(testSchemaFilePath);
+    // Keep only model schemas.
+    if (schemas) {
+        // OpenAPI 3
+        if (schemas.components &&
+            schemas.components.schemas) {
+            schemas = schemas.components.schemas;
+        }
+        // OpenAPI 2 (a.k.a. Swagger)
+        else if (schemas.definitions) {
+            schemas = schemas.definitions;
+        } else {
+            throw Error("Invalid input schema: no model schemas found");
+        }
+    }
 
-        // Keep only model schemas.
-        schemas = schemas.components.schemas;
+    // Remove keys not supported by JSON schema.
+    schemas = utils.removeKey(schemas, "example");
 
-        // Remove keys not supported by JSON schema.
-        schemas = utils.removeKey(schemas, "example");
+    // Remove unwanted keys.
+    // @todo refactor by reading from input arguments
+    schemas = utils.removeKey(schemas, "audits");
+    schemas = utils.removeKey(schemas, "Audit");
+    schemas = utils.removeKey(schemas, "Auditable");
 
-        // Remove unwanted keys.
-        schemas = utils.removeKey(schemas, "audits");
-        schemas = utils.removeKey(schemas, "Audit");
-        schemas = utils.removeKey(schemas, "Auditable");
+    for (let schema in schemas) {
 
-        for (let schema in schemas) {
-
-            if (schemas.hasOwnProperty(schema) === false) {
-                continue;
-            }
-
-            // Merge top-level allOf's of each schema.
-            schemas[schema] = mergeAllOf(schemas[schema], {ignoreAdditionalProperties: true});
-
-            // Convert each schema to JSON schema.
-            schemas[schema] = toJsonSchema(schemas[schema]);
-
+        if (schemas.hasOwnProperty(schema) === false) {
+            continue;
         }
 
-        console.log(schemas);
+        // Merge top-level allOf's of each schema.
+        schemas[schema] = mergeAllOf(schemas[schema], {ignoreAdditionalProperties: true});
 
-    } catch (err) {
-
-        console.error(err);
+        // Convert each schema to JSON schema.
+        schemas[schema] = toJsonSchema(schemas[schema]);
 
     }
+
+    console.log(schemas);
 
 })();
