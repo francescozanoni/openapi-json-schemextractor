9be80b15c11aa2c1e392feb274509f79e2cef22e
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-05-17 12:56:06 +0200

    refactored logic to separate function convert()

---
+++ src/convert.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const $RefParser = require("@apidevtools/json-schema-ref-parser");
+const toJsonSchema = require("@openapi-contrib/openapi-schema-to-json-schema");
+const mergeAllOf = require("json-schema-merge-allof");
+const utils = require("./utils");
+
+/**
+ * @param {String} inputSchemaPath
+ *
+ * @returns {Promise<*|$RefParser.JSONSchema>}
+ */
+module.exports = async function (inputSchemaPath) {
+
+    // Load input schema, parse to object and resolve all $ref's.
+    let schemas = await $RefParser.dereference(inputSchemaPath);
+
+    // Keep only model schemas.
+    if (schemas) {
+        // OpenAPI 3
+        if (schemas.components &&
+            schemas.components.schemas) {
+            schemas = schemas.components.schemas;
+        }
+        // OpenAPI 2 (a.k.a. Swagger)
+        else if (schemas.definitions) {
+            schemas = schemas.definitions;
+        } else {
+            throw Error("Invalid input schema: no model schemas found");
+        }
+    }
+
+    // Remove keys not supported by JSON schema.
+    schemas = utils.removeKey(schemas, "example");
+
+    // Remove unwanted keys.
+    // @todo refactor by reading from input arguments
+    schemas = utils.removeKey(schemas, "audits");
+    schemas = utils.removeKey(schemas, "Audit");
+    schemas = utils.removeKey(schemas, "Auditable");
+
+    for (let schema in schemas) {
+
+        if (schemas.hasOwnProperty(schema) === false) {
+            continue;
+        }
+
+        // Merge top-level allOf's of each schema.
+        schemas[schema] = mergeAllOf(schemas[schema], {ignoreAdditionalProperties: true});
+
+        // Convert each schema to JSON schema.
+        schemas[schema] = toJsonSchema(schemas[schema]);
+
+    }
+
+    return schemas;
+
+};
