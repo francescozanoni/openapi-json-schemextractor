260753be7ab3643e36e67ae7654b748981b725c9
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-05-18 10:53:27 +0200

    added first test of helpers.removeKeyFromObject

--- tests/unit.js
+++ tests/unit.js
@@ -1,5 +1,27 @@
 "use strict";
 
-test("first", () => {
-  expect("a").toBe("a");
+const helpers = require("../src/helpers");
+
+describe("Helpers", () => {
+
+    test("removeKeyFromObject", () => {
+        const before = {
+            a: 1,
+            b: 2,
+            c: {
+                a: 3,
+                b: 4
+            }
+        };
+        const after = {
+            a: 1,
+            c: {
+                a: 3
+            }
+        };
+        expect(helpers.removeKeyFromObject(before, "b")).toBe(after);
+    });
+
 });
+
+
