082148a6f279064418c365c32e6e4cf131bbdfd9
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-05-28 16:16:21 +0200

    minor fix

--- tests/file-path-manager.js
+++ tests/file-path-manager.js
@@ -5,12 +5,12 @@ const FilePathManager = require("../lib/file-path-manager");
 describe("isFilePathReadable", () => {
 
     test("yes", () => {
-        expect(FilePathManager.isFilePathReadable("../package.json"))
+        expect(FilePathManager.isFilePathReadable("./package.json"))
             .toStrictEqual(true);
     });
 
     test("no", () => {
-        expect(FilePathManager.isFilePathReadable("../a"))
+        expect(FilePathManager.isFilePathReadable("./a"))
             .toStrictEqual(false);
     });
 
